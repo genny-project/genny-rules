238a8869f2793b1e84e505c6c8b379de6e92176e
Jian Li <hbjoylee@gmail.com>
2020-06-19 14:12:57 +1000

    Remove sleep 100 ms

--- src/main/java/life/genny/rules/processor/RequestProcessor.java
+++ src/main/java/life/genny/rules/processor/RequestProcessor.java
@@ -1,15 +1,11 @@
 package life.genny.rules.processor;
 
-import io.vavr.Tuple2;
 import io.vavr.Tuple3;
-import life.genny.qwandautils.Tuple;
 import life.genny.rules.RulesLoader;
-import org.apache.commons.digester.Rules;
 import org.apache.logging.log4j.Logger;
 
 import java.lang.invoke.MethodHandles;
 import java.util.UUID;
-import java.util.concurrent.SynchronousQueue;
 
 public class RequestProcessor extends Thread {
     protected static final Logger log = org.apache.logging.log4j.LogManager
@@ -29,9 +25,11 @@ public class RequestProcessor extends Thread {
                 if (tuple != null) {
                     log.info("Process request uuid:" + tuple._3.toString() + ", RulesLoader instance:" + rulesLoader.toString());
                     rulesLoader.processMsg(tuple._1, tuple._2);
-                    Thread.sleep(100);
+//                    Thread.sleep(100);
                 }
-            } catch (InterruptedException | IllegalArgumentException e) {
+//            } catch (InterruptedException  ie) {
+//                log.error("Exception occurred: RulesLoader instance:" + rulesLoader.toString());
+            } catch (IllegalArgumentException e) {
                 log.error("Exception occurred: RulesLoader instance:" + rulesLoader.toString());
                 e.printStackTrace();
             }
