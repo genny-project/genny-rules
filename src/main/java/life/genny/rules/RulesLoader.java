f7c1bc39dd824bb393369fa1b9ae7d1e76feb504
Adam Crow <adamcrow63@gmail.com>
2020-08-07 02:13:47 +1000

    Added error check for null active-realms

--- src/main/java/life/genny/rules/RulesLoader.java
+++ src/main/java/life/genny/rules/RulesLoader.java
@@ -248,6 +248,11 @@ public class RulesLoader {
 		List<String> uninitialisedThemes = new ArrayList<String>();
 		List<String> uninitialisedFrames = new ArrayList<String>();
 
+		if (activeRealms == null) {
+			log.error(" ******** NO ACTIVE REALMS ******");
+			return false;
+		}
+		
 		for (String realm : activeRealms) {
 			log.info("LOADING " + realm + " RULES");
 			Integer rulesCount = setupKieRules(realm, rules);
