5e51441e7898b42095e7506681c952d4b43c374b
Jian Li <hbjoylee@gmail.com>
2020-08-07 16:12:06 +1000

    sort items by weight

--- src/main/java/life/genny/utils/DropdownUtils.java
+++ src/main/java/life/genny/utils/DropdownUtils.java
@@ -166,6 +166,34 @@ public class DropdownUtils implements Serializable {
 		return weight;
 	}
 
+	private  BaseEntity[] sortBaseEntityByWeight(BaseEntity[] items, BaseEntity parentBe) {
+		String parentCode = parentBe.getCode();
+		Set<EntityEntity> childLinks = parentBe.getLinks();
+		List<EntityEntity> sortedChildLinks = childLinks.stream()
+				.sorted(Comparator.comparing(EntityEntity::getWeight))
+				.collect(Collectors.toList());
+
+		BaseEntity[] newItems = new BaseEntity[items.length];
+
+		HashMap<String, BaseEntity> beMapping = new HashMap<>();
+		for (BaseEntity be : items)	 {
+			String beCode = be.getCode();
+			beMapping.put(beCode, be);
+		}
+
+		int index = 0;
+		for(EntityEntity ee :sortedChildLinks ) {
+			String targetCode = ee.getLink().getTargetCode();
+			if (beMapping.containsKey(targetCode)) {
+				newItems[index] = beMapping.get(targetCode);
+				index++;
+			} else {
+				log.error(String.format("Parent Code %s doesn't have Link code %s", parentCode, targetCode));
+			}
+		}
+		return newItems;
+	}
+
 	/*
 	 * Setting dynamic links between parents and child. ie. linking DropDown items
 	 * to the DropDown field.
@@ -174,20 +202,21 @@ public class DropdownUtils implements Serializable {
 			String linkValue, GennyToken gennyToken, Boolean sortByWeight) {
 		BaseEntity parentBe = new BaseEntityUtils(gennyToken).getBaseEntityByCode(parentCode);
 		if (parentBe != null) {
-			Set<EntityEntity> childLinks;
-			if (sortByWeight) {
-				childLinks = parentBe.getLinks();
-				List<EntityEntity> sortedChildLinks = childLinks.stream()
-						.sorted(Comparator.comparing(EntityEntity::getWeight))
-						.collect(Collectors.toList());
-				parentBe.setLinks(new LinkedHashSet<>(sortedChildLinks));
-			} else {
-				childLinks = new HashSet<>();
-				double index = -1.0;
-				/* creating a dumb attribute for linking the search results to the parent */
-				Attribute attributeLink = new Attribute(linkCode, linkCode, new DataType(String.class));
-				for (BaseEntity be : beMsg.getItems()) {
+			Set<EntityEntity> childLinks = new LinkedHashSet<>();
+			double index = -1.0;
+			/* creating a dumb attribute for linking the search results to the parent */
+			Attribute attributeLink = new Attribute(linkCode, linkCode, new DataType(String.class));
+
+			for (BaseEntity be : beMsg.getItems()) {
+				if (sortByWeight) {
+					//Sort items based on weight
+					BaseEntity[] sortedItems = sortBaseEntityByWeight(beMsg.getItems(), parentBe);
+					beMsg.setItems(sortedItems);
+					break;
+				} else {
 					index++;
+					childLinks = new HashSet<>();
+					parentBe.setLinks(childLinks);
 					EntityEntity ee = new EntityEntity(parentBe, be, attributeLink, index);
 					/* creating link for child */
 					Link link = new Link(parentCode, be.getCode(), attributeLink.getCode(), linkValue, index);
@@ -196,8 +225,8 @@ public class DropdownUtils implements Serializable {
 					/* adding child link to set of links */
 					childLinks.add(ee);
 				}
-				parentBe.setLinks(childLinks);
 			}
+			parentBe.setLinks(childLinks);
 			beMsg.add(parentBe);
 			return beMsg;
 		} else {
