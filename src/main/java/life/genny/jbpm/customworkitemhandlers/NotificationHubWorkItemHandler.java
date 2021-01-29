f5ba7813fb03a862fac97fef68366a5f6ba85c8b
Jasper Robison <jasper.robison22@gmail.com>
2021-01-29 12:20:36 +1100

    fix buggggggs

--- src/main/java/life/genny/jbpm/customworkitemhandlers/NotificationHubWorkItemHandler.java
+++ src/main/java/life/genny/jbpm/customworkitemhandlers/NotificationHubWorkItemHandler.java
@@ -6,6 +6,8 @@ import java.util.HashMap;
 import javax.mail.MessagingException;
 import javax.mail.internet.AddressException;
 
+import java.io.IOException;
+
 import org.apache.logging.log4j.Logger;
 import org.kie.api.runtime.process.WorkItem;
 import org.kie.api.runtime.process.WorkItemHandler;
@@ -15,6 +17,7 @@ import life.genny.models.GennyToken;
 import life.genny.notifications.EmailHelper;
 import life.genny.notifications.SmsHelper;
 import life.genny.qwanda.message.QBaseMSGMessageType;
+import life.genny.utils.BaseEntityUtils;
 
 /*
  * This workitem is for sending messages for JBPM Workflows.
@@ -39,40 +42,44 @@ public class NotificationHubWorkItemHandler implements WorkItemHandler {
     // extract parameters
     QBaseMSGMessageType messageType =
         (QBaseMSGMessageType) workItem.getParameter("notificationType");
-    String templateCode = (String) workItem.getParameter("templateID");
+    String template_id = (String) workItem.getParameter("templateID");
     String[] notificationRecipientArray = {
       (String) workItem.getParameter("notificationRecipientArray")
     };
-    HashMap<String, String> contextMap =
+    HashMap<String, String> templateData =
         (HashMap<String, String>) workItem.getParameter("templateData");
     GennyToken userToken = (GennyToken) workItem.getParameter("userToken");
 
+	BaseEntityUtils beUtils = new BaseEntityUtils(userToken);
+
     log.info("notificationType = " + messageType);
-    log.info("templateID = " + templateCode);
-    log.info("notificationRecipientArray = " + arrNotificationRecipient[0]);
-    log.info("templateData = " + contextMap);
+    log.info("templateID = " + template_id);
+    log.info("notificationRecipientArray = " + notificationRecipientArray[0]);
+    log.info("templateData = " + templateData);
     log.info("userToken = " + userToken);
 
-    String recipientEmail = null;
-    String recipientSms = null;
+	try {
+			
+		if (messageType.toString() == "EMAIL") {
 
-    if (messageType.toString() == "EMAIL") {
+			for (String recipientEmail : notificationRecipientArray) {
+				log.info("Sending EMAIL to " + recipientEmail);
+				EmailHelper.sendGrid(beUtils, recipientEmail, "", template_id, templateData);
+			}
 
-		for (String recipientEmail : notificationRecipientArray) {
-			log.info("Sending EMAIL to " + recipientEmail);
-			EmailHelper.sendGrid(beUtils, recipientEmail, "", template_id, templateData);
+		} else if (messageType.toString() == "SMS") {
+			
+			SmsHelper smsHelper = new SmsHelper();
+			String smsBody = templateData.get("smsBody");
+			for (String recipientSms : notificationRecipientArray) {
+				log.info("Sending SMS to " + recipientSms);
+				smsHelper.deliverSmsMsg(recipientSms, smsBody);
+			}
+
 		}
-
-    } else if (messageType.toString() == "SMS") {
-		
-		SmsHelper smsHelper = new SmsHelper();
-		String smsBody = templateData.get("smsBody");
-		for (String recipientSms : notificationRecipientArray) {
-			log.info("Sending SMS to " + recipientSms);
-			smsHelper.deliverSmsMsg(recipientSms, smsBody);
-		}
-
-    }
+	} catch (IOException e) {
+		e.printStackTrace();	
+	}
     // notify manager that work item has been completed
     manager.completeWorkItem(workItem.getId(), null);
   }
