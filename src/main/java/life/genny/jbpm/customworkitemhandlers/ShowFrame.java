9ba3929544e8ded82114595a4d58e42257aaf925
Jasper Robison <jasper.robison22@gmail.com>
2021-05-13 15:06:15 +1000

    length not size

--- src/main/java/life/genny/jbpm/customworkitemhandlers/ShowFrame.java
+++ src/main/java/life/genny/jbpm/customworkitemhandlers/ShowFrame.java
@@ -85,7 +85,7 @@ public class ShowFrame implements WorkItemHandler {
 		}
 
 		// Get filtered ask from the AskFilters ruleflow group
-		if (qBulkMessage.getAsks().size() > 0 && qBulkMessage.getAsks()[0].getItems().size() > 0) {
+		if (qBulkMessage.getAsks().length() > 0 && qBulkMessage.getAsks()[0].getItems().length() > 0) {
 			Ask filteredAsk = getAskFilters(beUtils, qBulkMessage.getAsks()[0].getItems()[0]);
 		}
 		// Send these filtered asks to overwrite
