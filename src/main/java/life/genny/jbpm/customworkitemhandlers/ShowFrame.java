24824b3a0920c63f687d077e9d787772ee17820d
Jasper Robison <jasper.robison22@gmail.com>
2021-05-31 12:03:52 +1000

    better null checking

--- src/main/java/life/genny/jbpm/customworkitemhandlers/ShowFrame.java
+++ src/main/java/life/genny/jbpm/customworkitemhandlers/ShowFrame.java
@@ -85,8 +85,8 @@ public class ShowFrame implements WorkItemHandler {
 		}
 
 		// Get filtered ask from the AskFilters ruleflow group
-		if (qBulkMessage.getAsks() != null && qBulkMessage.getAsks()[0].getItems() != null) {
-			if (qBulkMessage.getAsks().length > 0 && qBulkMessage.getAsks()[0].getItems().length > 0) {
+		if (qBulkMessage.getAsks() != null && qBulkMessage.getAsks().length > 0) {
+			if (qBulkMessage.getAsks()[0].getItems() != null && qBulkMessage.getAsks()[0].getItems().length > 0) {
 				Ask filteredAsk = getAskFilters(beUtils, qBulkMessage.getAsks()[0].getItems()[0]);
 			}
 		}
