b6c24986c632ed7c7af005b1bde25f26022bdc4d
Jasper Robison <jasper.robison22@gmail.com>
2021-07-06 09:00:39 +1000

    remove some logs

--- src/main/java/life/genny/utils/TableUtils.java
+++ src/main/java/life/genny/utils/TableUtils.java
@@ -631,14 +631,14 @@ public class TableUtils {
 							Optional<EntityAttribute> associateEa = associatedBe.findEntityAttribute(linkBeCode);
 							if (associateEa.isPresent()) {
 								String linkedValue = associatedBe.getValueAsString(linkBeCode);
-								log.info("CAL SEARCH linkedValue = " + linkedValue);
+								// log.info("CAL SEARCH linkedValue = " + linkedValue);
 								try {
 									Attribute primaryAttribute = RulesUtils.getAttribute(linkBeCode, serviceToken);
 									Answer ans = new Answer(be.getCode(), be.getCode(), calEA.getAttributeCode(), linkedValue);
 									Attribute att = new Attribute("_" +attributeCode + "__" + linkBeCode, primaryAttribute.getName(), primaryAttribute.getDataType());
 									/*att.setCode("PRI_" +attributeCode + "__" + linkBeCode);*/
 									/*att.setCode(linkBeCode);*/
-									log.info("The CAL att after is "+att);
+									// log.info("The CAL att after is "+att);
 									ans.setAttribute(att);
 									be.addAnswer(ans);
 								} catch (BadDataException e) {
