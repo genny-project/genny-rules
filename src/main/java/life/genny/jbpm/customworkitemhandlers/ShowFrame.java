0e2317b8efa373e7c853646707843f5aa4f37e25
Jasper Robison <jasper.robison22@gmail.com>
2021-08-03 22:39:57 +1000

    log

--- src/main/java/life/genny/jbpm/customworkitemhandlers/ShowFrame.java
+++ src/main/java/life/genny/jbpm/customworkitemhandlers/ShowFrame.java
@@ -486,6 +486,7 @@ public class ShowFrame implements WorkItemHandler {
 
 								// Check Dependencies, and disable if not met
 								Boolean dependenciesMet = beUtils.dependenciesMet(dropdownCode, target, defBe);
+								log.info("dependenciesMet = " + dependenciesMet);
 								if (dependenciesMet != null && !dependenciesMet) {
 									if (updated.getItems() != null && updated.getItems().length > 0 && updated.getItems()[0] != null) {
 										for (Ask childAsk : updated.getItems()[0].getChildAsks()) {
